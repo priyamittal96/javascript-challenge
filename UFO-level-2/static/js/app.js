db941edb2563a2980ca767330462f91c289d7ad1
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-03-27 14:36:22 -0700

    Updating filters

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,7 +17,20 @@ function buildTable(data) {
 
 let filters = {}
 function updateFilters(){
+    console.log(d3.select(this))
+    let changeElement = d3.select(this).select("input");
 
+    let elementValue = changeElement.property("value");
+
+    let filterID = changeElement.attr("id")
+    
+    if(elementValue){
+        filters[filterID] = elementValue
+    }
+    else {
+        delete filters[filterID]
+    }
+    filterTable()
 }
 
 // Attach an event to listen for changes to each filter 
